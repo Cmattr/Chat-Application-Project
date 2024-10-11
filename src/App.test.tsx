143d586f54db5a65410ddf9e5ba32720ea89b775
Cmattr <cmattr@outlook.com>
2024-10-11 12:06:03 -0400

    updated test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders HomePage correctly', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+
+  // Looking for "Sign in to open Chat" header text
+  const signInHeader = screen.getByText(/sign in to open chat/i);
+  expect(signInHeader).toBeInTheDocument();
 });
